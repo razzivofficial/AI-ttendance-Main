e9dc9d43f82062319a6347710134ae6da4ccf29b
razzivofficial <razzivofficial@gmail.com>
2024-04-24 23:43:22 +0530

    bugs-removed

--- student.py
+++ student.py
@@ -239,7 +239,7 @@ class Face_Rec_System:
         showAll_btn=Button(Search_frame,text="Show All",width=12,font=('times new roman', 13,'bold'),bg='blue',fg='white')
         showAll_btn.grid(row=0,column=4,padx=5)
         
-        #===============Table Frame===============#
+       #===============Table Frame===============#
 
         table_frame = Frame(Right_frame,bd=2,bg='white',relief="ridge")
         table_frame.place(x=5, y=210, width=725, height=345)
@@ -247,14 +247,11 @@ class Face_Rec_System:
         scroll_x=ttk.Scrollbar(table_frame,orient=HORIZONTAL)
         scroll_y=ttk.Scrollbar(table_frame,orient=VERTICAL)
 
-        self.student_table = ttk.Treeview(table_frame, columns=("col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9", "col10", "col11", "col12", "col13", "col14", "col15", "col16", "col17", "col18", "col19", "col20"), xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
+        self.student_table = ttk.Treeview(table_frame, columns=("col1", "col2", "col3", "col4", "col5", "col6", "col7", "col8", "col9", "col10", "col11", "col12", "col13", "col14"), xscrollcommand=scroll_x.set, yscrollcommand=scroll_y.set)
         scroll_x.pack(side=BOTTOM,fill=X)
         scroll_y.pack(side=RIGHT,fill=Y)
         scroll_x.config(command=self.student_table.xview)
         scroll_y.config(command=self.student_table.yview)
-
-        
-        
 
         self.student_table.heading("col1", text="Department")
         self.student_table.heading("col2", text="Course")
@@ -273,10 +270,9 @@ class Face_Rec_System:
 
         self.student_table["show"] = "headings"
 
+        self.student_table.column("col1", width=100)
 
-        self.student_table.column("dept",width=100)
-
-        self.student_table.pack(fill=BOTH, expand=True)  
+        self.student_table.pack(fill=BOTH, expand=True)
 
 
 if __name__ == "__main__":
